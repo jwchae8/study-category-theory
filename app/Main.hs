1ddf50fd97e3cdce0ec87ca23590b01400d14c6b
Jongwook Chae <jwchae8@gmail.com>
2019-05-28 14:10:05 +0900

    Added some test suite

--- app/Main.hs
+++ app/Main.hs
@@ -2,7 +2,14 @@ module Main where
 
 import Lib
 
+double :: Int -> Int
+double x = x * 2
+
+addOne :: Int -> Int
+addOne x = x + 1
+
 main :: IO ()
 main = do 
     print (myId 5)
     print (myId 10)
+    print (compose double addOne 4)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,21 @@
 import Lib
+import Test.Hspec
+
+inc :: Int -> Int
+inc x = x + 1
+
+dec :: Int -> Int
+dec x = x - 1
 
 main :: IO ()
-main = print (myId 5 == 5)
+main = hspec $ do
+  describe "Prelude.head" $ do
+    it "Identity function" $ do
+      myId 5 `shouldBe` 5
+      myId "asdf" `shouldBe` "asdf"
+    it "Compose function" $ do
+      compose inc dec 5 `shouldBe` 5
+      compose inc inc 5 `shouldBe` 7
+    it "Composition repects identity" $ do
+      fid inc 5 `shouldBe` 6
+      idf inc 5 `shouldBe` 6
